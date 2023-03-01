a752459e83ca24fdba17d8b6afbef351cc665190
gitexpert <101324884+lilingkay@users.noreply.github.com>
2023-03-01 12:42:52 +0800

    Update main.tf

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -1,3 +1,42 @@
+resource "aws_security_group" "create_websg" {
+
+  name        = var.asg.asg_sg_name
+  description = var.asg.asg_sg_description
+  vpc_id      = var.vpc_id_pass
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.req_tags, {
+    Name = var.asg.asg_sg_name
+    }
+  )
+}
+
+#security group web rule
+resource "aws_security_group_rule" "create_web_rule_http" {
+  type                     = "ingress"
+  from_port                = var.web_http_rule.from_port
+  to_port                  = var.web_http_rule.to_port
+  protocol                 = var.web_http_rule.protocol
+  source_security_group_id = var.albsg_id_pass
+  security_group_id        = aws_security_group.create_websg.id
+}
+
+resource "aws_security_group_rule" "create_web_rule_ssh" {
+  type                     = "ingress"
+  from_port                = var.web_ssh_rule.from_port
+  to_port                  = var.web_ssh_rule.to_port
+  protocol                 = var.web_ssh_rule.protocol
+  source_security_group_id = var.bastionsg_id_pass
+  security_group_id        = aws_security_group.create_websg.id
+}
+
+
 #create launch configuration
 resource "aws_launch_configuration" "create_launchconfig" {
   name                        = var.asg.launchconfig_name
